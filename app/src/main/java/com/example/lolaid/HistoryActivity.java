d6d41015fc2dce0500071ead045f2752ccac5102
TheAlexet <thealexet6@gmail.com>
2021-04-15 20:11:50 +0200

    Navigation among tabs done

---
+++ app/src/main/java/com/example/lolaid/HistoryActivity.java
@@ -0,0 +1,24 @@
+package com.example.lolaid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HistoryActivity extends Fragment {
+
+    public HistoryActivity() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.history_activity, container, false);
+    }
+
+}
--- app/src/main/java/com/example/lolaid/MainActivity.java
+++ app/src/main/java/com/example/lolaid/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.lolaid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void changeFragment(View view) {
+        Class<? extends Fragment> fragmentClass = null;
+        if (view.getId() == R.id.bStats) {
+            fragmentClass = StatsActivity.class;
+        } else if(view.getId() == R.id.bHistory) {
+            fragmentClass = HistoryActivity.class;
+        } else if(view.getId() == R.id.bRecommendations) {
+            fragmentClass = RecommendationsActivity.class;
+        } else if(view.getId() == R.id.bLiveGame) {
+            fragmentClass = LiveGameActivity.class;
+        }
+        if(fragmentClass != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fcvView, fragmentClass, null)
+                    .commit();
+        }
+    }
+
 }
