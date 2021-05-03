8f01c218688b61084b7aec1336cd8d05250c6f86
marcgf5 <45403823+marcgf5@users.noreply.github.com>
2021-05-03 20:22:59 +0200

    Live game window and adapter made and improved

---
+++ app/src/main/java/adapters/LiveGameAdapter.java
@@ -0,0 +1,60 @@
+package adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lolaid.R;
+
+import java.util.List;
+
+import business_logic.data_models.CurrentGameParticipant;
+
+public class LiveGameAdapter extends RecyclerView.Adapter<LiveGameAdapter.ViewHolder>{
+
+    private static List<CurrentGameParticipant> participantList;
+
+    public LiveGameAdapter(List<CurrentGameParticipant> pList){
+        participantList = pList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.live_game_player_row, parent, false);
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        CurrentGameParticipant participant = this.participantList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView championIcon;
+        private TextView summonerName;
+        private ImageView summSpell1;
+        private ImageView summSpell2;
+        private ImageView runesMain;
+        private ImageView runesSecondary;
+
+        public ViewHolder(View v){
+            super(v);
+            championIcon = v.findViewById(R.id.championPortrait);
+            summonerName = v.findViewById(R.id.summonerName);
+            summSpell1 = v.findViewById(R.id.summSpell1);
+            summSpell2 = v.findViewById(R.id.summSpell2);
+            runesMain = v.findViewById(R.id.runesMain);
+            runesSecondary = v.findViewById(R.id.runesSecondary);
+        }
+    }
+}
